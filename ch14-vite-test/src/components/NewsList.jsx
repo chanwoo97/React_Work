d47c34d468feaaa33031e44181ce68dd60d25380
chanwoo97 <jcw6223@naver.com>
2025-08-19 17:33:12 +0900

    React 9일차 수업

--- ch14-vite-test/src/components/NewsList.jsx
+++ ch14-vite-test/src/components/NewsList.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import usePromise from '../lib/usePromise.jsx';
+import PdItemBusan from './PdItemBusan.jsx';
 
 //css 작업
 const NewsListBlock = styled.div`
@@ -17,50 +19,74 @@ const NewsListBlock = styled.div`
     padding-right: 1rem;
   }
 `;
-// 더미 샘플 데이터 이용.
-const sampleArticle = {
-  title: '제목',
-  description: '내용',
-  url: 'http://example.com/',
-  urlToImage:
-    'https://sportshub.cbsistatic.com/i/r/2025/08/16/253debd9-29a2-48cd-8b31-501631451ee8/thumbnail/1200x675/db792d9aa7d3d3d9326347b93bd6439c/dricus-khamzat-weighins.jpg',
-};
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   //실제 데이터 연동,
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(false);
+  // 커스텀 훅스 사용하기전, -> 리팩토링 하기전,
+  // NewsList_backup2.jsx , 파일 참고,
 
-  //
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=us&apiKey=본인키값',
-        );
-        setArticles(response.data.articles);
-        console.log(response.data);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
+  // 공공데이터 추가 작업 2
+  const sendData = () => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    console.log(`category 1 : ${category}`);
+    if (category === 'busanAtt') {
+      return axios.get(
+        `https://apis.data.go.kr/6260000/AttractionService/getAttractionKr?serviceKey=ALRX9GpugtvHxcIO%2FiPg1vXIQKi0E6Kk1ns4imt8BLTgdvSlH%2FAKv%2BA1GcGUQgzuzqM3Uv1ZGgpG5erOTDcYRQ%3D%3D&numOfRows=100&pageNo=1&resultType=json`,
+      );
+    } else {
+      return axios.get(
+        `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=aac32bdd5ce74c918028f526a4315511
+`,
+      );
+    }
+  };
+
+  // 변경 전
+  // const [loading, response, error] = usePromise(() => {
+  //   const query = category === 'all' ? '' : `&category=${category}`;
+  //   return axios.get(
+  //     `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=b7adb4f936494b3bac62f446ab7686cb`,
+  //   );
+  // }, [category]);
+
+  // 공공데이터 추가 작업 3
+  const [loading, response, error] = usePromise(sendData, [category]);
+
+  // 대기 중
+  if (loading) {
+    return <NewsListBlock>대기중입니다.</NewsListBlock>;
+  }
+  // response 값이 설정이 안됐을 경우,
+  if (!response) {
+    return null;
+  }
+
+  // 에러가 발생할수도 있음.
+  if (error) {
+    return <NewsListBlock>에러 발생</NewsListBlock>;
+  }
+  // 정상 값을 받을 때.
+  // 변경 전
+  // const { articles } = response.data;
+
+  // 공공데이터 추가 작업 4,
+  // 데이터 구조를 반드시 확인 후,
+  const data =
+    category === 'busanAtt'
+      ? response.data.getAttractionKr.item || []
+      : response.data.articles || [];
 
   return (
     <NewsListBlock>
-      {/*실제 데이터 연동*/}
-      {articles.map((article) => (
-        <NewsItem key={article.url} article={article} />
-      ))}
+      {/*변경전*/}
+      {/*{articles.map((article) => (*/}
+      {/*  <NewsItem key={article.url} article={article} />*/}
+      {/*))}*/}
 
-      {/*<NewsItem article={sampleArticle} />*/}
-      {/*<NewsItem article={sampleArticle} />*/}
-      {/*<NewsItem article={sampleArticle} />*/}
-      {/*<NewsItem article={sampleArticle} />*/}
-      {/*<NewsItem article={sampleArticle} />*/}
+      {/*공공데이터 추가 작업 5*/}
+      {category === 'busanAtt'
+        ? data.map((data, index) => <PdItemBusan key={index} article={data} />)
+        : data.map((data) => <NewsItem article={data} />)}
     </NewsListBlock>
   );
 };
